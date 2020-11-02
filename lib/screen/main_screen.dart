3a962c502d159143fe6193106d6d696386eb30d4
Fedor B Gorsky <fedor@support-pc.org>
2020-11-02 18:05:34 +0300

    tune tree list

--- lib/screen/main_screen.dart
+++ lib/screen/main_screen.dart
@@ -3,6 +3,9 @@ import 'package:sshstudio/widgets/tabbed_area.dart';
 import 'package:sshstudio/widgets/tree_list.dart';
 
 class MainScreen extends StatelessWidget {
+
+  final double bottomButtonsHeight = 100;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -10,14 +13,98 @@ class MainScreen extends StatelessWidget {
         child: Row(
           children: [
             Container(
-              width: 200,
-              child: TreeList(),
+              width: 400,
+            decoration: BoxDecoration(
+                border: Border.all(color: Colors.blueAccent)
+            ),
+              child:  Column(
+                  children:[
+                    SingleChildScrollView(
+                      child: Container(
+                        height: MediaQuery.of(context).size.height - bottomButtonsHeight - 2, // 200,
+                        // width: 400,
+                        child: TreeList(),
+                        ),
+                    ),
+
+                    Container(
+                      height: bottomButtonsHeight,
+                      child: Row(
+                        children: [
+                          IconButton(
+                            color: Colors.green,
+                            icon: Icon(Icons.add),
+                            onPressed: () {
+                              showDialog(
+                                  context: context,
+                                  builder: (BuildContext context) {
+                                    return _addWindow(context);
+                                  });
+                            },
+                          ),
+                        ],
+                      ),
+                    )
+
+                    //
+                  ]
+              ),
             ),
             Expanded(
               child:TabbedArea(),
             )
           ],
         ),
+      ),
+    );
+  }
+
+
+
+  Widget _addWindow(context) {
+    return AlertDialog(
+      content: Stack(
+        children: <Widget>[
+          Form(
+            // key: _formKey,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: TextFormField(),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: TextFormField(),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: RaisedButton(
+                    child: Text("Submit"),
+                    onPressed: () {
+                      // if (_formKey.currentState.validate()) {
+                      //   _formKey.currentState.save();
+                      // }
+                    },
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: RaisedButton(
+                    child: Text("Close"),
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                      // if (_formKey.currentState.validate()) {
+                      //   _formKey.currentState.save();
+                      // }
+                    },
+                  ),
+                )
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
--- lib/widgets/tree_list.dart
+++ lib/widgets/tree_list.dart
@@ -24,21 +24,8 @@ class _TreeListState extends State<TreeList> {
   @override
   Widget build(BuildContext context) {
     return TreeView(
+      hasScrollBar: true,
       parentList: [
-        Parent(
-          parent: IconButton(
-            color: Colors.green,
-            icon: Icon(Icons.add),
-            onPressed: () {
-              showDialog(
-                  context: context,
-                  builder: (BuildContext context) {
-                    return _addWindow(context);
-                  });
-            },
-          ),
-          childList: ChildList(),
-        ),
         Parent(
           parent: _folder('root', 5),
           childList: ChildList(
@@ -95,54 +82,6 @@ class _TreeListState extends State<TreeList> {
             ),
           ],
         ),
-      ),
-    );
-  }
-
-  Widget _addWindow(context) {
-    return AlertDialog(
-      content: Stack(
-        children: <Widget>[
-          Form(
-            // key: _formKey,
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: <Widget>[
-                Padding(
-                  padding: EdgeInsets.all(8.0),
-                  child: TextFormField(),
-                ),
-                Padding(
-                  padding: EdgeInsets.all(8.0),
-                  child: TextFormField(),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: RaisedButton(
-                    child: Text("Submit"),
-                    onPressed: () {
-                      // if (_formKey.currentState.validate()) {
-                      //   _formKey.currentState.save();
-                      // }
-                    },
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: RaisedButton(
-                    child: Text("Close"),
-                    onPressed: () {
-                      Navigator.of(context).pop();
-                      // if (_formKey.currentState.validate()) {
-                      //   _formKey.currentState.save();
-                      // }
-                    },
-                  ),
-                )
-              ],
-            ),
-          ),
-        ],
       ),
     );
   }
