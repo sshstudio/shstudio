c119bbb9a706b6657d59ae85ce9f375fae486a71
Fedor B Gorsky <fedor@support-pc.org>
2020-11-18 19:55:32 +0300

    split_view cursor

--- lib/screen/main_screen.dart
+++ lib/screen/main_screen.dart
@@ -1,9 +1,9 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
-import 'package:split_view/split_view.dart';
 import 'package:sshstudio/utils/constants.dart';
 import 'package:sshstudio/widgets/single_child_scroll_view_with_scrollbar.dart';
+import 'package:sshstudio/widgets/split_view.dart';
 import 'package:sshstudio/widgets/tabbed_area.dart';
 import 'package:sshstudio/widgets/tree_list.dart';
 
